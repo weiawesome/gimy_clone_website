b66d3defb0fa52b3772df90c81f60b34c066ba1d
tcweeei <open891013@gmail.com>
2024-04-07 14:15:41 +0800

    Add the model of film create

---
+++ src/model/request/film_create.ts
@@ -0,0 +1,12 @@
+export type RequestCreateFilm={
+    title:string
+    state:string
+    type:string
+    category:string
+    actors:string[]
+    directors:string[]
+    location:string
+    releaseYear:number
+    introduction:string
+    language:string
+}
