6303e94d29ae09fe0a600675d1b61316eb28d830
tcweeei <open891013@gmail.com>
2024-04-07 14:24:14 +0800

    Remove not using import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
