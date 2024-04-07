258ac66eb8e345eea49efa058232126ce2120cf6
tcweeei <open891013@gmail.com>
2024-04-07 14:18:09 +0800

    Add the service for get filter film

---
+++ src/service/get_filter_films.ts
@@ -0,0 +1,27 @@
+import {config} from "@/service/config";
+
+export async function GetFilterFilms(film_type:string,film_category:string|string[]|undefined,location:string|string[]|undefined,releaseYear:string|string[]|undefined,orderType:string,page:number){
+    let url=config.SERVICE_URL+config.ROUTE_FILTER+film_type+"?"+"order_type="+orderType
+    const offset=(page-1)*config.PAGE_SIZE
+    const limit=config.PAGE_SIZE
+
+    url+="&"+"offset="+offset
+    url+=+"&"+"limit="+limit
+
+    if (film_category!==undefined){
+        url+="&"+"category="+String(film_category)
+    }
+    if (location!==undefined){
+        url+="&"+"location="+String(location)
+    }
+    if (releaseYear!=undefined){
+        url+="&"+"release_year="+String(releaseYear)
+    }
+
+    return await fetch(url)
+        .then(response => response.json())
+        .then((data: ResponseFilmList) => {
+            return data
+        })
+        .catch(error => console.error('Error:', error));
+}
