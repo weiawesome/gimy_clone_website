bde000fb21b674318bcc1e1f0751d6499a548f5b
tcweeei <open891013@gmail.com>
2024-04-07 14:18:49 +0800

    Add the service for upload ad

---
+++ src/service/upload_ad.ts
@@ -0,0 +1,23 @@
+import {config} from "@/service/config";
+import {AdType} from "@/model/repsonse/ad_information";
+
+export async function UploadAd(file: File, advertisementType: AdType, expiredTime: string): Promise<void> {
+    const url=config.SERVICE_URL+config.ROUTE_UPLOAD_AD
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("advertisement_type", advertisementType);
+    formData.append("expired_time", expiredTime);
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: formData,
+        });
+        if (!response.ok) {
+            const errorResponse = await response.json(); // 假設服務器返回的是 JSON 格式的錯誤資訊
+            throw new Error(JSON.stringify(errorResponse));
+        }
+    } catch (error) {
+        throw error
+    }
+}
