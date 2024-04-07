1f6a067260d0b55e62017dbcf698767dbefaa7fc
tcweeei <open891013@gmail.com>
2024-04-07 14:16:38 +0800

    Add the model of film information response

---
+++ src/model/repsonse/film_information.ts
@@ -0,0 +1,15 @@
+type ResponseFilmInformation ={
+    id:string
+    title:string
+    resource:string
+    state:string
+    category:string
+    actors:string[]
+    directors:string[]
+    location:string
+    releaseYear:number
+    updateTime:string
+    type:string
+    popularity:number
+    introduction:string
+}
