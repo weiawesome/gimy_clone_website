453a864316c90749206b13d420c7834dec25df0a
tcweeei <open891013@gmail.com>
2024-04-16 16:57:40 +0800

    Edit the text display way

--- src/components/film_full_information.tsx
+++ src/components/film_full_information.tsx
@@ -17,18 +17,18 @@ const FilmFullInformation:React.FC<FilmInformationProps>=({filmInformation,film_
                     <p>&nbsp;-&nbsp;</p>
                     <p>{film_route}</p>
                 </div>
-                <div className={"flex flex-row justify-between w-1/2 text-normal-color text-sm mt-0.5"}>
-                    <div className={"flex flex-row"}>
+                <div className={"flex flex-row justify-start overflow-hidden truncate text-normal-color text-sm mt-0.5"}>
+                    <div className={"flex flex-row mr-3"}>
                         <p>類別&nbsp;:&nbsp;</p>
                         <Link href={GetTypeRouteFormCategory(filmInformation?.category)+"?category="+filmInformation?.category} className={"font-bold hover:text-primary-color"}>{FormatCategory(filmInformation?.category)}</Link>
                     </div>
-                    <p className={"font-bold"}>|</p>
-                    <div className={"flex flex-row"}>
+                    <p className={"font-bold mr-3"}>|</p>
+                    <div className={"flex flex-row mr-3"}>
                         <p>年分&nbsp;:&nbsp;</p>
                         <Link href={GetTypeRouteFormCategory(filmInformation?.category)+"?releaseYear="+filmInformation?.releaseYear} className={"font-bold hover:text-primary-color"}>{filmInformation?.releaseYear}</Link>
                     </div>
-                    <p className={"font-bold"}>|</p>
-                    <div className={"flex flex-row"}>
+                    <p className={"font-bold mr-3"}>|</p>
+                    <div className={"flex flex-row mr-3"}>
                         <p>人氣&nbsp;:&nbsp;</p>
                         <p className={"font-bold"}>{filmInformation?.popularity}</p>
                     </div>
