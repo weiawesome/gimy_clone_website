5847980ebf41991fae8fdc470072b316e7c1c999
tcweeei <open891013@gmail.com>
2024-05-16 01:47:45 +0800

    Edit to SSR

--- src/pages/resource/[film_id]/[film_route]/index.tsx
+++ src/pages/resource/[film_id]/[film_route]/index.tsx
@@ -1,11 +1,10 @@
 "use client"
-import React, {useEffect, useState} from "react";
-import {useRouter} from "next/router";
+import React from "react";
 import TitleBar from "@/components/title_bar";
 import {config} from "@/service/config"
 import ScrollToTopButton from "@/components/scroll_to_top_button";
 import "@/app/globals.css"
-import {GetFilmInformation} from "@/service/get_film_information";
+import {GetFilmInformation, GetFilmRoutesInformation} from "@/service/get_film_information";
 import FilmRoutesInformation from "@/components/film_routes_information";
 import HLSVideoPlayer from "@/components/HLSVideoPlayer";
 import EditEpisodeButton from "@/components/edit_episode_button";
@@ -15,16 +14,35 @@ import {CategoryMapping} from "@/data/category";
 import AdInformation from "@/components/ad_information";
 import {AdType} from "@/model/repsonse/ad_information";
 import Head from "next/head";
+import {GetServerSideProps} from "next";
 
-const FilmResourcePage:React.FC=()=>{
-    const router = useRouter();
-    const { film_id,film_route,episode } = router.query;
-    const [filmInformation,setFilmInformation]=useState<ResponseFilmInformation>()
+export interface FilmResourceInformationProps{
+    film_id:string
+    film_route:string
+    episode:string
+    filmInformation:ResponseFilmInformation
+    filmRoutesInformation:ResponseFilmRoutesInformation
+}
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { query } = context;
+    const film_id = query.film_id;
+    const film_route = query.film_route;
+    const episode = query.episode;
+    if(typeof film_id!=='string' || typeof film_route!=='string' || typeof episode!=='string'){
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    }
 
-    useEffect(() => {
-        GetFilmInformation(String(film_id)).then(r => setFilmInformation(r!))
-    }, [film_id,film_route,episode]);
+    const filmInformation=await GetFilmInformation(film_id)
+    const filmRoutesInformation=await GetFilmRoutesInformation(film_id)
+    return {props:{film_id,film_route,episode,filmInformation,filmRoutesInformation}};
+};
 
+const FilmResourcePage:React.FC<FilmResourceInformationProps>=({film_id,film_route,episode,filmInformation,filmRoutesInformation})=>{
     return (
         <main>
             <Head>
@@ -37,12 +55,12 @@ const FilmResourcePage:React.FC=()=>{
             }></TitleBar>
             <ScrollToTopButton></ScrollToTopButton>
             <div className={"bg-center items-center"}>
-                <HLSVideoPlayer src={config.SERVICE_URL+config.ROUTE_MEDIA_LIST+film_route+"/"+film_id+"/"+episode} prepare={film_route!==undefined && film_id!==undefined && episode!==undefined}/>
+                <HLSVideoPlayer src={config.SERVICE_URL+config.ROUTE_MEDIA_LIST+film_route+"/"+film_id+"/"+episode}/>
                 <EditEpisodeButton film_id={film_id} film_route={film_route} episode={episode}></EditEpisodeButton>
             </div>
-            <FilmFullInformation film_route={String(film_route)} episode={String(episode)} filmInformation={filmInformation!}></FilmFullInformation>
+            <FilmFullInformation film_route={film_route} episode={episode} filmInformation={filmInformation}></FilmFullInformation>
             <AdInformation adType={AdType.WEB_BAR}></AdInformation>
-            <FilmRoutesInformation chose_state={true} chosen_film_route={String(film_route)} chosen_episode={String(episode)} film_id={String(film_id)}></FilmRoutesInformation>
+            <FilmRoutesInformation chose_state={true} chosen_film_route={film_route} chosen_episode={String(episode)} film_id={film_id} information={filmRoutesInformation}></FilmRoutesInformation>
             <Footer></Footer>
         </main>
     )
--- src/pages/resource/[film_id]/index.tsx
+++ src/pages/resource/[film_id]/index.tsx
@@ -1,9 +1,8 @@
-import React, {useEffect, useState} from "react";
-import {useRouter} from "next/router";
+import React from "react";
 import TitleBar from "@/components/title_bar";
 import ScrollToTopButton from "@/components/scroll_to_top_button";
 import FilmInformation from "@/components/film_information";
-import {GetFilmInformation} from "@/service/get_film_information";
+import {GetFilmInformation, GetFilmRoutesInformation} from "@/service/get_film_information";
 import FilmRoutesInformation from "@/components/film_routes_information";
 import "@/app/globals.css"
 import Sub_title from "@/components/sub_title";
@@ -15,15 +14,30 @@ import AdInformation from "@/components/ad_information";
 import {AdType} from "@/model/repsonse/ad_information";
 import RankingSideList from "@/components/ranking_side_list";
 import Head from "next/head";
+import {GetServerSideProps} from "next";
 
-const FilmInformationPage:React.FC=()=>{
-    const router = useRouter();
-    const { film_id } = router.query;
-    const [filmInformation,setFilmInformation]=useState<ResponseFilmInformation>()
+interface FilmInformationProps{
+    film_id:string
+    filmInformation:ResponseFilmInformation
+    filmRoutesInformation:ResponseFilmRoutesInformation
+}
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { query } = context;
+    const film_id = query.film_id;
+    if(typeof film_id!=='string'){
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    }
 
-    useEffect(() => {
-        GetFilmInformation(String(film_id)).then(r => setFilmInformation(r!))
-    }, [film_id]);
+    const filmInformation=await GetFilmInformation(film_id)
+    const filmRoutesInformation=await GetFilmRoutesInformation(film_id)
+    return {props:{film_id,filmInformation,filmRoutesInformation}};
+};
+const FilmInformationPage:React.FC<FilmInformationProps>=({film_id,filmInformation,filmRoutesInformation})=>{
 
     return (
         <main>
@@ -36,10 +50,10 @@ const FilmInformationPage:React.FC=()=>{
                 CategoryMapping[filmInformation!.category].index:CategoryMapping.ALL_CATEGORY.index
             }></TitleBar>
             <ScrollToTopButton></ScrollToTopButton>
-            <FilmInformation filmInformation={filmInformation!}></FilmInformation>
+            <FilmInformation film_id={film_id} filmRoutesInformation={filmRoutesInformation} filmInformation={filmInformation!} ></FilmInformation>
             <div className={"flex justify-between"}>
                 <div className={"flex w-full flex-col lg:w-2/3"}>
-                    <FilmRoutesInformation chose_state={false} chosen_film_route={""} chosen_episode={""} film_id={String(film_id)}></FilmRoutesInformation>
+                    <FilmRoutesInformation chose_state={false} chosen_film_route={""} chosen_episode={""} film_id={film_id} information={filmRoutesInformation}></FilmRoutesInformation>
                     <AdInformation adType={AdType.WEB_BAR}></AdInformation>
                     <div className={"w-full m-3"}>
                         <p className={"text-normal-color font-bold text-md"}>劇情介紹</p>
