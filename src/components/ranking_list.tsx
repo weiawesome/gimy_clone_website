9c27d9958026db5bae1461b2d5afe9fbadc516ed
tcweeei <open891013@gmail.com>
2024-05-16 01:51:43 +0800

    Edit the word not change line

--- src/components/ranking_list.tsx
+++ src/components/ranking_list.tsx
@@ -25,14 +25,14 @@ const RankingList:React.FC<RankingListProps>=({base_url,category})=>{
                 return (
                     <div className={"mt-2.5"} key={index}>
                         <div className={"flex flex-row justify-between items-center"}>
-                            <Link href={"/resource/"+item.id} className={"flex flex-1 items-center text-normal-color hover:text-primary-color"}>
+                            <Link href={"/resource/"+item.id} className={"mr-2.5 overflow-hidden truncate flex items-center text-normal-color hover:text-primary-color"}>
                                 {index+1<=3?
                                     <p className={"p-0.5 pl-4 pr-3 rounded-l-xl text-sm bg-primary-color text-reverse-color"}>{index+1}</p>:
                                     <p className={"p-0.5 pl-4 pr-3 rounded-l-xl text-sm bg-reverse-color text-normal-color"}>{index+1}</p>
                                 }
-                                <p className={"ml-3 text-sm"}>{item.title}</p>
+                                <p className={"truncate ml-2.5 text-sm"}>{item.title}</p>
                             </Link>
-                            <p className={"flex-1 text-end text-opacity-50 text-plain-color"}>{item.popularity}</p>
+                            <p className={"text-end text-opacity-50 text-plain-color"}>{item.popularity}</p>
                         </div>
                     </div>
                 )
