6b5593ec4ace2b84519ce8a7c61cb1a48132a806
tcweeei <open891013@gmail.com>
2024-05-16 01:50:38 +0800

    Remove the display of popularity

--- src/components/ranking_side_list.tsx
+++ src/components/ranking_side_list.tsx
@@ -28,7 +28,7 @@ const RankingSideList:React.FC<RankingSideListProps>=({base_url,category})=>{
         <div className={"flex flex-col pr-3 pb-3"}>
             <Sub_title title={FormatCategory(category)+"排行榜"}></Sub_title>
             {firstFilmInformation!==undefined &&(
-                <div className={"relative mt-2.5 flex justify-between"}>
+                <div className={"relative mt-2.5 flex justify-between truncate"}>
                     <p className={"absolute pl-2 pr-2 p-1 rounded-tl-lg text-sm bg-primary-color text-reverse-color z-30"}>{1}</p>
                     <Link href={"resource/"+firstFilmInformation.id} className={"w-1/2 relative"}>
                         <Image width={200} height={250} src={loadError?"/icon.png":firstFilmInformation.resource} alt={"image"} className={"bg-reverse-color shadow-lg rounded-lg w-full h-full"} onError={()=>{setLoadError(true)}}></Image>
@@ -37,9 +37,9 @@ const RankingSideList:React.FC<RankingSideListProps>=({base_url,category})=>{
                         </div>
                     </Link>
                     <div className={"truncate text-sm text-normal-color items-start w-1/2 pl-3"}>
-                        <p className={"text-lg font-bold"}>{firstFilmInformation.title}</p>
-                        <p className={"mt-3"}>{firstFilmInformation.actors.join(",")}</p>
-                        <p className={"mt-1"}>{firstFilmInformation.state}</p>
+                        <p className={"truncate text-md font-bold"}>{firstFilmInformation.title}</p>
+                        <p className={"mt-5 truncate"}>{firstFilmInformation.actors.join(",")}</p>
+                        <p className={"mt-1 truncate"}>{firstFilmInformation.state}</p>
                     </div>
 
                 </div>
@@ -57,9 +57,8 @@ const RankingSideList:React.FC<RankingSideListProps>=({base_url,category})=>{
                                     <p className={"p-0.5 pl-4 pr-3 rounded-l-xl text-sm bg-primary-color text-reverse-color"}>{index+1}</p>:
                                     <p className={"p-0.5 pl-4 pr-3 rounded-l-xl text-sm bg-reverse-color text-normal-color"}>{index+1}</p>
                                 }
-                                <p className={"ml-3 text-sm"}>{item.title}</p>
+                                <p className={"ml-3 text-sm truncate"}>{item.title}</p>
                             </Link>
-                            <p className={"flex-1 text-end text-opacity-50 text-plain-color"}>{item.popularity}</p>
                         </div>
                     </div>
                 )
