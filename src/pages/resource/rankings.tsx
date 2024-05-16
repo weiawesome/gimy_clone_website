935ae0a3e629960fdf0704be3f755666833670f2
tcweeei <open891013@gmail.com>
2024-05-16 11:07:16 +0800

    Add api-url parameter for fetching data

--- src/pages/resource/rankings.tsx
+++ src/pages/resource/rankings.tsx
@@ -11,11 +11,18 @@ import "@/app/globals.css"
 import Footer from "@/components/footer";
 import RankingList from "@/components/ranking_list";
 import {
-    AnimateAnimate, MovieDrama,TvSeriesHKSeries,
+    AnimateAnimate, MovieDrama, TvSeriesHKSeries,
     TvSeriesJPSeries, TvSeriesKRSeries, TvSeriesTWSeries, TvSeriesUSSeries
 } from "@/data/category";
 import Head from "next/head";
-const RankingsPage:React.FC=()=>{
+import {GetServerSideProps} from "next";
+import {config} from "@/service/config";
+import {ClientServiceProps} from "@/data/utils";
+export const getServerSideProps: GetServerSideProps = async () => {
+    const serviceUrl=config.API_SERVICE_URL
+    return {props: {serviceUrl}};
+};
+const RankingsPage:React.FC<ClientServiceProps>=({serviceUrl})=>{
     return (
         <main>
             <Head>
@@ -25,14 +32,14 @@ const RankingsPage:React.FC=()=>{
             <ScrollToTopButton></ScrollToTopButton>
             <p className={"text-center text-normal-color font-bold text-3xl mt-5 mb-10"}>本週維劇迷排行</p>
             <div className={"grid grid-cols-1 lg:grid-cols-3 gap-4"}>
-                <RankingList base_url={AnimateTypeInformation.route} category={AnimateAnimate.QUERY_CATEGORY}></RankingList>
-                <RankingList base_url={TvSeriesTypeInformation.route} category={TvSeriesKRSeries.QUERY_CATEGORY}></RankingList>
-                <RankingList base_url={TvSeriesTypeInformation.route} category={TvSeriesUSSeries.QUERY_CATEGORY}></RankingList>
-                <RankingList base_url={TvSeriesTypeInformation.route} category={TvSeriesJPSeries.QUERY_CATEGORY}></RankingList>
-                <RankingList base_url={TvSeriesTypeInformation.route} category={TvSeriesTWSeries.QUERY_CATEGORY}></RankingList>
-                <RankingList base_url={TvSeriesTypeInformation.route} category={TvSeriesHKSeries.QUERY_CATEGORY}></RankingList>
-                <RankingList base_url={AnimateTypeInformation.route} category={AnimateAnimate.QUERY_CATEGORY}></RankingList>
-                <RankingList base_url={MovieTypeInformation.route} category={MovieDrama.QUERY_CATEGORY}></RankingList>
+                <RankingList service_url={serviceUrl} base_url={AnimateTypeInformation.route} category={AnimateAnimate.QUERY_CATEGORY}></RankingList>
+                <RankingList service_url={serviceUrl} base_url={TvSeriesTypeInformation.route} category={TvSeriesKRSeries.QUERY_CATEGORY}></RankingList>
+                <RankingList service_url={serviceUrl} base_url={TvSeriesTypeInformation.route} category={TvSeriesUSSeries.QUERY_CATEGORY}></RankingList>
+                <RankingList service_url={serviceUrl} base_url={TvSeriesTypeInformation.route} category={TvSeriesJPSeries.QUERY_CATEGORY}></RankingList>
+                <RankingList service_url={serviceUrl} base_url={TvSeriesTypeInformation.route} category={TvSeriesTWSeries.QUERY_CATEGORY}></RankingList>
+                <RankingList service_url={serviceUrl} base_url={TvSeriesTypeInformation.route} category={TvSeriesHKSeries.QUERY_CATEGORY}></RankingList>
+                <RankingList service_url={serviceUrl} base_url={AnimateTypeInformation.route} category={AnimateAnimate.QUERY_CATEGORY}></RankingList>
+                <RankingList service_url={serviceUrl} base_url={MovieTypeInformation.route} category={MovieDrama.QUERY_CATEGORY}></RankingList>
             </div>
             <Footer></Footer>
         </main>
