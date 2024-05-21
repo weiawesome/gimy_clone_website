95a1ffb7ffc4de2cfc29a4f28c9375fbbe5feb63
tcweeei <open891013@gmail.com>
2024-05-21 09:27:34 +0800

    Edit the display content of error message

--- src/components/load_error.tsx
+++ src/components/load_error.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import "@/app/globals.css"
-import Image from "next/image";
 
 const LoadError: React.FC = () => {
     return (
         <div className={"absolute h-full w-full flex justify-center"}>
-            <div className="relative flex flex-col justify-around self-center items-center w-1/2 h-1/2">
-                <Image priority={false} src={"/icon.png"} alt={"icon"} width={200} height={200}/>
-                <p className={"text-reverse-color text-xl font-bold"}>載入出現些許錯誤 請稍後再嘗試</p>
+            <div className="relative flex self-center items-center">
+                <p className={"text-reverse-color lg:text-3xl text-xl font-bold"}>載入出現些許錯誤 請稍後再嘗試</p>
             </div>
         </div>
 
