ae650c9f727c3e4050920179e20d99a130efba4c
tcweeei <open891013@gmail.com>
2024-05-21 09:28:09 +0800

    Edit the size and position of waving blocks

--- src/components/loading_wave.tsx
+++ src/components/loading_wave.tsx
@@ -5,11 +5,11 @@ const LoadingWave: React.FC = () => {
     const colors = ['bg-red-500', 'bg-orange-500', 'bg-yellow-500', 'bg-green-500', 'bg-blue-500'];
     return (
         <div className={"absolute h-full w-full flex justify-center"}>
-            <div className="relative flex justify-around self-center items-center w-1/2 h-full">
+            <div className="relative flex justify-center self-center items-center h-full">
                 {colors.map((item, index) => (
                     <div
                         key={index}
-                        className={"w-5 h-5 rounded "+item}
+                        className={"m-5 w-5 h-5 rounded "+item}
                         style={{
                             animation: `wave 1.2s infinite ease-in-out ${index * 0.15}s`,
                         }}
